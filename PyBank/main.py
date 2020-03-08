5faa2e06ee86326942f9b061c6b0929e6820ae62
elc17 <neto@lopezcenteno.com>
2020-03-07 19:33:27 -0600

    Finshed HW

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,8 +4,6 @@ import csv
 
 # Header for statistics
 import statistics
-# Header for pandas
-import pandas as pd
 
 #  Read the file
 bank_csv = r"C:\Users\Ernesto\Desktop\elearning\BootCamp\TDM-REV-DATA-PT-01-2020-U-C\02-Homework\03-Python\Instructions\PyBank\Resources\budget_data.csv"
@@ -14,8 +12,14 @@ bank_csv = r"C:\Users\Ernesto\Desktop\elearning\BootCamp\TDM-REV-DATA-PT-01-2020
     
 # Counter for the amount of months
 count = 0
+
+# Counter for the total amount of profit/losses
 total = 0
+
+# List for the average profit/losses
 average = []
+
+# Variables to calculate the greatest and lowest months
 max_month = 0
 min_month = 0
 greatest = 0
@@ -23,27 +27,54 @@ gmonth = 0
 lowest = 0
 lmonth = 0
 
+# Creates a text file and writes
 f = open("results.txt","w+")
 
+# Opens as csv file
 with open(bank_csv) as csvfile:
+    
+    # Variable to read the csv file and separate it with ","
     budgetcsv = csv.reader(csvfile,delimiter = ',')
+    
+    # Skips the header
     next(budgetcsv, None)
     
+    # For loop  to calculate the required outputs
     for row in budgetcsv:
+        
+        # Adds 1 to the counter of transactions 
         count = count + 1
+        
+        # Turns the current profit/loss value into integer
         totals = row[1]
+        
+        # Adds the current profit/loss to the total profits
         total = total + int(totals)
+        
+        # Adds the profit/loss to the list to calculate the average
         average.append(int(row[1]))
         
+        # If to figure out the greatest and lowest month
+        # If the current profit is larger than the greatest until now
         if int(row[1]) > greatest:
+            
+            # Changes the greatest to the current profit
             greatest = int(row[1])
+            
+            # Changes the greatest month to the current month
             gmonth = row[0]
+            
+        # If the current loss is larger than the biggest until now
         elif int(row[1]) < lowest:
+            
+            # Changes the lowest to the current loss
             lowest = int(row[1])
+            
+            # Changes the lowest month to the current month
             lmonth = row[0]
         
         
-
+# Print results
 print("Financial Analysis")
 print("--------------------------------------------------")
 print("Total months:", count)
@@ -52,6 +83,7 @@ print("Average change:", round(statistics.mean(average)))
 print("Greatest increase in profits:", gmonth, "(" + str(greatest) + ")")
 print("Greatest decrease in profits:", lmonth, "(" + str(lowest) + ")")
 
+# Writes the results into the .txt file
 f.write("Financial Analysis\n")
 f.write("-------------------------------------------------\n")
 f.write("Total months: %i\n"%count)
@@ -63,4 +95,6 @@ f.write(")\n")
 f.write("Greatest decrease in profits: %s"%lmonth)
 f.write("(%i"%lowest)
 f.write(")")
+
+# Closes the .txt file
 f.close
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,16 +8,25 @@ import statistics
 # Header for counter:
 from collections import Counter
 
+# Variable for the total amount of votes
 total = 0
+
+# Dictionary for key candidate : value votes
 candidate = {}
 
 #  Read the file
 votes_csv = r"C:\Users\Ernesto\Desktop\elearning\BootCamp\TDM-REV-DATA-PT-01-2020-U-C\02-Homework\03-Python\Instructions\PyPoll\Resources\election_data.csv"
 
+# Create a .txt file to write on
 f = open("poll.txt", "w+")
 
+# Use as csv file
 with open(votes_csv) as csvfile:
+    
+    # Reads the csv file and separates with ","
     votescsv = csv.reader(csvfile,delimiter = ',')
+    
+    # Skips the header
     next(votescsv, None)
     
     # For loop to calculate the amoun of votes
@@ -26,26 +35,41 @@ with open(votes_csv) as csvfile:
         # Adds votes to get the total amount
         total += 1
         
+        # If the current candidate doesn't exist in the dictionary
         if row[2] not in candidate.keys():
-                candidate[row[2]] = 1
+            
+            # Creates the key and sets value to 1
+            candidate[row[2]] = 1
         else:
-                candidate[row[2]] = candidate[row[2]] + 1
-                
+            
+            # If the key exists in the dictionary, add 1 to the value
+            candidate[row[2]] = candidate[row[2]] + 1
+
+# Dictionary for key candidate : value percentage    
 percentage = {}
+
+# Prints the first answers
 print("Election Results")
 print("--------------------------------------------------")
 print("Total votes:", total)
 print("--------------------------------------------------")
 
+# Writes the first answers to the .txt file
 f.write("Election Results\n")
 f.write("--------------------------------------------------\n")
 f.write("Total votes: %i\n"%total)
 f.write("--------------------------------------------------\n")
 
+# For loop to turn the vote numbers into percentages
 for key in candidate:
+    
+    # Rounds the percentage calculation on each candidate vote value for each key
     percentage[key] = round((candidate[key]/total)*100,2)
     
+    # Prints the results
     print(str(key) + ":", str(percentage[key]) + "%", "(" + str(candidate[key]) + ")")
+    
+    # Writes the results into the .txt file
     f.write(str(key))
     f.write(":")
     f.write(str(percentage[key]))
@@ -54,10 +78,20 @@ for key in candidate:
     f.write(str(candidate[key]))
     f.write(")\n")
 
+# Prints the line
 print("--------------------------------------------------")
 f.write("--------------------------------------------------\n")
+
 #winnerv = max(candidate.values())
+
+# Get the candidate with the maximum votes
 winnerk = max(candidate, key = candidate.get)
 
+# Print the answer
 print("Winner:", winnerk)
+
+# Write the answer into a .txt file
 f.write("Winner:%s"%winnerk)
+
+# Closes the .txt file
+f.close
